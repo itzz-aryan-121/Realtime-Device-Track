9af515d0e54e63813b70c07e56e53da82281e5c4
Aryan <aryantomar239@gmail.com>
2024-09-20 18:02:28 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const path = require('path');
+
+
+const socketio = require('socket.io');
+
+const server = http.createServer(app);
+
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+    socket.on('sendLocation', function(data)  {
+        io.emit('receiveLocation', {id: socket.id, ...data});
+    });
+socket.on('disconnect', function(){
+  io.emit('user-disconnected', socket.id);
+});
+});
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
